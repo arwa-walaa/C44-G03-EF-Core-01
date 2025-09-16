aae2dc0b256047cbb40c29221bd996beac116ca6
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-09-16 21:16:41 +0300

    Session04-05 Mapping Inheritance [TPCT]

---
+++ InhertanceMapping/Context/CompanyContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InhertanceMapping.Context
+{
+    internal class CompanyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ARWA\\SQLEXPRESS01; Database= CompanyRoute2; Trusted_Connection=True; TrustServerCertificate=True; ")
+             ; 
+        }
+
+        #region Table Per Concrete Type [TPCT]
+
+        public DbSet<FullTimeEmployee> FullTimeEmployees { get; set; }
+        public DbSet<PartTimeEmployee> PartTimeEmployees { get; set; }
+
+
+        #endregion
+    }
+}
---
+++ InhertanceMapping/Program.cs
@@ -0,0 +1,49 @@
+﻿using InhertanceMapping.Context;
+
+namespace InhertanceMapping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using CompanyContext context = new CompanyContext();
+            #region Inhertence Mapping
+
+            #region Table Per Concrete Type [TPCT]
+
+            //FullTimeEmployee fte = new FullTimeEmployee()
+            //{
+            //    Name = "Arwa",
+            //    Address = "Cairo",
+            //    Age = 30,
+            //    Salary = 10000,
+            //    StartDate = DateTime.Now
+            //};
+            //PartTimeEmployee pte = new PartTimeEmployee()
+            //{
+            //    Name = "Mohamed",
+            //    Address = "Giza",
+            //    Age = 28,
+            //    HourlyRate = 100,
+            //    CountOfHours = 160
+            //};
+
+            //context.FullTimeEmployees.Add(fte);
+            //context.PartTimeEmployees.Add(pte);
+            //context.SaveChanges();
+
+            var FET = (from fe in context.FullTimeEmployees
+                       select fe).FirstOrDefault();
+            var PTE = context.PartTimeEmployees.FirstOrDefault();
+             Console.WriteLine($"FTE: {FET.Name}, Salary: {FET.Salary}, StartDate: {FET.StartDate}");   
+                Console.WriteLine($"PTE: {PTE.Name}, HourlyRate: {PTE.HourlyRate}, CountOfHours: {PTE.CountOfHours}");
+
+
+
+            #endregion
+
+            #endregion
+
+        }
+    }
+}
