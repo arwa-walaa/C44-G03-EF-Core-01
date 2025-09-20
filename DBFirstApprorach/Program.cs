96a675c8464190f340fe40a77550de9776044248
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-09-20 18:14:55 +0300

    Session05-02 DB First With Command

---
+++ DBFirstApprorach/Program.cs
@@ -0,0 +1,37 @@
+﻿using DBFirstApprorach.Contexts;
+
+namespace DBFirstApprorach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region BD first With Command 
+
+            /*
+             * Download Package Manager Console
+             * - Install-Package Microsoft.EntityFrameworkCore.SqlServer
+             * - Install-Package Microsoft.EntityFrameworkCore.Tools
+             * - Install-Package Microsoft.EntityFrameworkCore.Proxies
+             * 
+             * 2- use DB Northwid =>Scaffold =>package Manager Console
+             
+             */
+
+            //get all products name 
+
+            using MyNorthwindDbContext myNorthwindDbContext = new MyNorthwindDbContext();
+            var products = myNorthwindDbContext.Products.ToList();
+
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            #endregion
+
+
+        }
+    }
+}
--- InhertanceMapping/Program.cs
+++ InhertanceMapping/Program.cs
@@ -6,147 +6,147 @@ namespace InhertanceMapping
     {
         static void Main(string[] args)
         {
-            using CompanyContext context = new CompanyContext();
-            #region Inhertence Mapping
+            //using CompanyContext context = new CompanyContext();
+            //#region Inhertence Mapping
 
-            #region Table Per Concrete Type [TPCT]
+            //#region Table Per Concrete Type [TPCT]
 
-            //FullTimeEmployee fte = new FullTimeEmployee()
-            //{
-            //    Name = "Arwa",
-            //    Address = "Cairo",
-            //    Age = 30,
-            //    Salary = 10000,
-            //    StartDate = DateTime.Now
-            //};
-            //PartTimeEmployee pte = new PartTimeEmployee()
-            //{
-            //    Name = "Mohamed",
-            //    Address = "Giza",
-            //    Age = 28,
-            //    HourlyRate = 100,
-            //    CountOfHours = 160
-            //};
+            ////FullTimeEmployee fte = new FullTimeEmployee()
+            ////{
+            ////    Name = "Arwa",
+            ////    Address = "Cairo",
+            ////    Age = 30,
+            ////    Salary = 10000,
+            ////    StartDate = DateTime.Now
+            ////};
+            ////PartTimeEmployee pte = new PartTimeEmployee()
+            ////{
+            ////    Name = "Mohamed",
+            ////    Address = "Giza",
+            ////    Age = 28,
+            ////    HourlyRate = 100,
+            ////    CountOfHours = 160
+            ////};
 
-            //context.FullTimeEmployees.Add(fte);
-            //context.PartTimeEmployees.Add(pte);
-            //context.SaveChanges();
+            ////context.FullTimeEmployees.Add(fte);
+            ////context.PartTimeEmployees.Add(pte);
+            ////context.SaveChanges();
 
-            //var FET = (from fe in context.FullTimeEmployees
-            //           select fe).FirstOrDefault();
-            //var PTE = context.PartTimeEmployees.FirstOrDefault();
-            // Console.WriteLine($"FTE: {FET.Name}, Salary: {FET.Salary}, StartDate: {FET.StartDate}");   
-            //    Console.WriteLine($"PTE: {PTE.Name}, HourlyRate: {PTE.HourlyRate}, CountOfHours: {PTE.CountOfHours}");
-
-
-
-            #endregion
-
-            #region Table Per Hierarchy [TPH]
-
-            //FullTimeEmployee fte = new FullTimeEmployee()
-            //{
-            //    Name = "Arwa",
-            //    Address = "Cairo",
-            //    Age = 30,
-            //    Salary = 10000,
-            //    StartDate = DateTime.Now
-            //};
-            //PartTimeEmployee pte = new PartTimeEmployee()
-            //{
-            //    Name = "Mohamed",
-            //    Address = "Giza",
-            //    Age = 28,
-            //    HourlyRate = 100,
-            //    CountOfHours = 160
-            //};
-
-            //context.Add(fte);
-            //context.Add(pte);
-            //context.SaveChanges();
-
-            //var Emps = from E in context.Employees
-            //          select E;
-
-            //foreach (var emp in Emps.OfType<FullTimeEmployee>())
-            //    Console.WriteLine(emp.Name);
+            ////var FET = (from fe in context.FullTimeEmployees
+            ////           select fe).FirstOrDefault();
+            ////var PTE = context.PartTimeEmployees.FirstOrDefault();
+            //// Console.WriteLine($"FTE: {FET.Name}, Salary: {FET.Salary}, StartDate: {FET.StartDate}");   
+            ////    Console.WriteLine($"PTE: {PTE.Name}, HourlyRate: {PTE.HourlyRate}, CountOfHours: {PTE.CountOfHours}");
 
 
 
+            //#endregion
 
+            //#region Table Per Hierarchy [TPH]
 
-            #endregion
-            #region Table Per Type [TPT]
-            //FullTimeEmployee fte = new FullTimeEmployee()
-            //{
-            //    Name = "Arwa",
-            //    Address = "Cairo",
-            //    Age = 30,
-            //    Salary = 10000,
-            //    StartDate = DateTime.Now
-            //};
-            //PartTimeEmployee pte = new PartTimeEmployee()
-            //{
-            //    Name = "Mohamed",
-            //    Address = "Giza",
-            //    Age = 28,
-            //    HourlyRate = 100,
-            //    CountOfHours = 160
-            //};
+            ////FullTimeEmployee fte = new FullTimeEmployee()
+            ////{
+            ////    Name = "Arwa",
+            ////    Address = "Cairo",
+            ////    Age = 30,
+            ////    Salary = 10000,
+            ////    StartDate = DateTime.Now
+            ////};
+            ////PartTimeEmployee pte = new PartTimeEmployee()
+            ////{
+            ////    Name = "Mohamed",
+            ////    Address = "Giza",
+            ////    Age = 28,
+            ////    HourlyRate = 100,
+            ////    CountOfHours = 160
+            ////};
 
-            //context.Add(fte);
-            //context.Add(pte);
-            //context.SaveChanges();
+            ////context.Add(fte);
+            ////context.Add(pte);
+            ////context.SaveChanges();
 
-            //var Emps = (from E in context.Employees
-            //           select E).ToList();
-            //foreach(var E in Emps.OfType<PartTimeEmployee>()) {
-            //    Console.WriteLine(E.Name);
-            //}
+            ////var Emps = from E in context.Employees
+            ////          select E;
 
-            #endregion
-
-            #region Local
-
-            #region Ex01
+            ////foreach (var emp in Emps.OfType<FullTimeEmployee>())
+            ////    Console.WriteLine(emp.Name);
 
 
 
-            //var Emps = context.Employees.Any(E=>E.Age != null);
-
-            //Console.WriteLine(Emps);
-
-            ////local
-            //var Emps = context.Employees.Local.Any(E => E.Age != null);
-            //Console.WriteLine(Emps);
-            #endregion
-
-            #region Ex02
 
 
+            //#endregion
+            //#region Table Per Type [TPT]
+            ////FullTimeEmployee fte = new FullTimeEmployee()
+            ////{
+            ////    Name = "Arwa",
+            ////    Address = "Cairo",
+            ////    Age = 30,
+            ////    Salary = 10000,
+            ////    StartDate = DateTime.Now
+            ////};
+            ////PartTimeEmployee pte = new PartTimeEmployee()
+            ////{
+            ////    Name = "Mohamed",
+            ////    Address = "Giza",
+            ////    Age = 28,
+            ////    HourlyRate = 100,
+            ////    CountOfHours = 160
+            ////};
 
-            //var Emps = context.Employees.Any(E=>E.Age != null);
+            ////context.Add(fte);
+            ////context.Add(pte);
+            ////context.SaveChanges();
 
-            //Console.WriteLine(Emps);
-            //var Emps01 = context.Employees.FirstOrDefault();
-            //if(Emps01 != null)
-            //{
-            //    Console.WriteLine(Emps01.Age);
-            //    Emps01.Age = null;
-            //}
+            ////var Emps = (from E in context.Employees
+            ////           select E).ToList();
+            ////foreach(var E in Emps.OfType<PartTimeEmployee>()) {
+            ////    Console.WriteLine(E.Name);
+            ////}
 
-            //var Emps = context.Employees.Any(E => E.Age != null);
-            //Console.WriteLine($"DB {Emps}" );
-            ////local
-            //var LocalEmps = context.Employees.Local.Any(E => E.Age != null);
-            //Console.WriteLine(LocalEmps);
-            #endregion
+            //#endregion
 
-            #endregion
+            //#region Local
+
+            //#region Ex01
 
 
 
-            #endregion
+            ////var Emps = context.Employees.Any(E=>E.Age != null);
+
+            ////Console.WriteLine(Emps);
+
+            //////local
+            ////var Emps = context.Employees.Local.Any(E => E.Age != null);
+            ////Console.WriteLine(Emps);
+            //#endregion
+
+            //#region Ex02
+
+
+
+            ////var Emps = context.Employees.Any(E=>E.Age != null);
+
+            ////Console.WriteLine(Emps);
+            ////var Emps01 = context.Employees.FirstOrDefault();
+            ////if(Emps01 != null)
+            ////{
+            ////    Console.WriteLine(Emps01.Age);
+            ////    Emps01.Age = null;
+            ////}
+
+            ////var Emps = context.Employees.Any(E => E.Age != null);
+            ////Console.WriteLine($"DB {Emps}" );
+            //////local
+            ////var LocalEmps = context.Employees.Local.Any(E => E.Age != null);
+            ////Console.WriteLine(LocalEmps);
+            //#endregion
+
+            //#endregion
+
+
+
+            //#endregion
         }
 
     }
